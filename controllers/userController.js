8248bc51ef78c03b619d8afa653700f1de5acc55
33kumarram <95571520+33kumarram@users.noreply.github.com>
2023-05-20 17:22:41 +0530

    updated error messages

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,30 +10,28 @@ const sendOtp = asyncHandler(async (req, res) => {
 
   if (!email) {
     res.status(400);
-    throw new Error("Please enter your email id");
+    throw new Error("Missing Email ID parameter. Please provide your Email ID to proceed");
   }
 
   const existing_mail = await users.findOne({ email });
   let wrong_attempts;
 
   if (existing_mail && existing_mail.wrong_attempts >= 5) {
-    // incase if user made subsequently five wrong attemps it will block him for one hour
+    // incase if user made subsequently five wrong attemps, his/her account will be blocked for one hour.
     if (Date.now() - new Date(existing_mail.wrong_otp_submitted_at).getTime() <=3600000) {
       let try_after_minutes = 60 -Math.floor((Date.now() - new Date(existing_mail.wrong_otp_submitted_at).getTime()) / 60000);
       res.status(400);
-      throw new Error(
-        `You have entered five wrong otp please try again after ${try_after_minutes} minute`
-      );
+      throw new Error(`Your account has been blocked due to five consecutive incorrect OTP entries. Please try again after ${try_after_minutes} minute`);
     } else {
-      // if user came after one hour of five consecutive wrong attempts it will reset wrong attempts to 0 and unblock him
+      // If a user attempts five consecutive wrong OTPs and returns after one hour, the wrong attempt count will be reset to 0, and the user will be unblocked.
       wrong_attempts = 0;
     }
   }
-  // it prevent user from regenerating otp within one minute of prvious otp 
+  // it restrict user from regenerating otp within one minute of prvious otp 
   if( existing_mail&&Date.now() - new Date(existing_mail.otp_generated_at).getTime() <60000){
     let seconds = 60-Math.floor((Date.now() - new Date(existing_mail.otp_generated_at).getTime())/1000)
     res.status(400);
-      throw new Error(`Wait for ${seconds} second and then try to resend otp`);
+      throw new Error(`You are regenerating the OTP too quickly. Please try again after ${seconds} second`);
   }
 
   try {
@@ -50,12 +48,12 @@ const sendOtp = asyncHandler(async (req, res) => {
     let info = await transporter.sendMail({
       from: process.env.EMAIL, // sender address
       to: email, // list of receivers
-      subject: "Log In OTP", // Subject line
-      text: `Dear user your six digit OTP to login to Ramesh app is ${otp}. Don't share it with anyone`, // plain text body
+      subject: `Login OTP for Ramesh's App - Confidential Information`, // Subject line
+      text: `Dear user, We have generated a six-digit OTP for you to log in to Ramesh's app. Your OTP is ${otp}. Please keep it confidential and do not share it with anyone`, // plain text body
       // html: "<b>Hello world?</b>", // html body
     });
     
-    // if user is not unblocked by us then it will not change wrong attempts value
+    // If the wrong attempt count is not reset to 0, the system will save the current wrong attempt count for future reference.
     if (wrong_attempts!==0) {
       wrong_attempts = existing_mail?.wrong_attempts;
     }
@@ -71,15 +69,15 @@ const sendOtp = asyncHandler(async (req, res) => {
         wrong_attempts: wrong_attempts,
       },
       {
-        upsert: true,     //to create new collection if it don't find collection for mentioned mail id
+        upsert: true,     //If user details for the mentioned email ID is not found, a new user will be created.
       }
     );
     // console.log("Message sent: %s", info.messageId);
-    res.status(201).json(`OTP sent to the mail Id: ${email}`);
+    res.status(201).json(`Login OTP sent to the mail Id: ${email}`);
   } catch (err) {
     console.log(err);
     res.status(400);
-    throw new Error("Error occurred while sending OTP");
+    throw new Error("Some error occurred while generating OTP. Please try again after some time");
   }
 });
 
@@ -87,44 +85,43 @@ const logIn = asyncHandler(async function (req, res) {
   const { email, otp } = req.body;
   if (!email || !otp || otp === "") {
     res.status(400);
-    throw new Error("Email Id or OTP missing from params");
+    throw new Error("Missing Email ID or OTP parameter. Please provide both your Email ID and OTP to proceed.");
   }
 
   const user = await users.findOne({ email });
 
   if (!user || !user.otp) {
     res.status(400);
-    throw new Error("OTP not generated yet");
+    throw new Error("OTP details not found on the server. Please try regenerating the OTP again");
   }
   
-  // incase if user submitted subsequently five wrong otp it will block him for one hour
+  // If a user submits five consecutive wrong OTPs, he/she will be blocked for one hour.
   if (user.wrong_attempts >= 5 &&Date.now() - new Date(user.wrong_otp_submitted_at).getTime() <=3600000) {
     let try_after_minutes =60 -Math.floor((Date.now() - new Date(user.wrong_otp_submitted_at).getTime()) / 60000);
     res.status(400);
-    throw new Error(
-      `You have entered five wrong otp please try again after ${try_after_minutes} minute`
-    );
+    throw new Error(`Your account has been blocked due to five consecutive incorrect OTP entries. Please try again after ${try_after_minutes} minute`);
   }
 
   if (`${otp}` === `${user.otp}`) {
-    //check whether otp has been expired or not
+    //It checks whether otp has been expired or not. OTP get expired after 5 minutes
     if (Date.now() - new Date(user.otp_generated_at).getTime() >= 300000) {
       res.status(400);
-      throw new Error("OTP has been expired");
+      throw new Error("The OTP has expired. Please request a new OTP.");
     }
-    // if not expird then remove otp, equate wrong attempts to 0 and send jsonwebtoken in response
+    // If OTP is not expird then remove otp from saved data, equate wrong attempts to 0 and send jsonwebtoken in response
     await users.findByIdAndUpdate(user._id, { otp: "", wrong_attempts: 0 });
     res.status(201).json({
-      token: generateToken(user._id),
+      token: generateToken(user._id),  
     });
   } else {
-    // increase wrong attemp and update wrong otp submission time when user submit a invalid otp
+    // When a user submits an invalid OTP, the system increases the wrong attempt count and updates the timestamp of the last wrong OTP submission.
     let updated_user = await users.findByIdAndUpdate(user._id, {
       $inc: { wrong_attempts: 1 },
       wrong_otp_submitted_at:new Date()
     });
+    let chances_left = 5-updated_user.wrong_attempts
     res.status(400);
-    throw new Error("Invalid OTP");
+    throw new Error(`Invalid OTP entered. You have ${chances_left} chances left. After that, your account will be blocked for one hour. Please make sure to enter the correct OTP.`);
   }
 });
 
